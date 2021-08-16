4b91284f4056fad46fec027e005bc0f3e919e871
Helder M Adversi Jr <helderadversi@gmail.com>
2021-08-16 10:07:41 -0300

    core: creates project

---
+++ MemeMe v1/MemeMe v1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMe v1
+//
+//  Created by Helder Marcelo Adversi Junio on 16/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
