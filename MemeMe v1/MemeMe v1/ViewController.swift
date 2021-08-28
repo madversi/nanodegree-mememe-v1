2e83238dd24d2fcc3ac9361eb87bbee684f85195
Helder M Adversi Jr <helderadversi@gmail.com>
2021-08-28 15:43:51 -0300

    feat: adds toolbar into the main screen

--- MemeMe v1/MemeMe v1/ViewController.swift
+++ MemeMe v1/MemeMe v1/ViewController.swift
@@ -2,18 +2,34 @@
 //  ViewController.swift
 //  MemeMe v1
 //
-//  Created by Helder Marcelo Adversi Junio on 16/08/21.
+//  Created by Helder M Adversi Jr on 16/08/21.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var imagePickerView: UIImageView!
+
+    @IBAction func pickAnImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController, animated: true, completion: nil)
     }
-
-
 }
 
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        let alert = UIAlertController(title: "Ops", message: "Have u canceled?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: { _ in
+            self.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.cropRect] as? UIImage {
+            imagePickerView.image = image
+        }
+    }
+}
